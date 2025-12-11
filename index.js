1d92ece1d53271fd35fab2e9609387eda103b1f4
AgustinCardozo <agus.cardozo96@gmail.com>
2025-12-11 16:55:06 -0300

    feat: soporte para notFoundMiddleware

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 import "dotenv/config";
+import bodyParser from 'body-parser';
+import cors from 'cors';
 import express from "express";
+
+import { notFoundMiddleware } from "./src/middlewares/notFound.middleware.js";
 import productsRouter from "./src/routes/products.router.js";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors());
+app.use(bodyParser.json());
 app.use("/api", productsRouter);
+app.use(notFoundMiddleware);
 
 app.get("/", (req, res) => {
   res.json({ message: "API Rest en Node.js" });
